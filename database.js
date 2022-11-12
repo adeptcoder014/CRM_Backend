9e212f066f265be0a98dbd9f12b6909e0bde4a8b
Nischal <gupta.nischal014@gmail.com>
2022-11-12 16:05:18 +0530

    commit

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 //===========================
 // const DB="mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority"
-
+console.log("--->",process.env.DB_URL)
 mongoose.connect(process.env.DB_URL,{
     useNewUrlParser : true,
     useUnifiedTopology : true
