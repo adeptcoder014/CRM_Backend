4cafdd22aecd095274e0707d3701ab0414d35eec
Nischal <gupta.nischal014@gmail.com>
2022-11-12 16:02:03 +0530

    commit

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 //===========================
 // const DB="mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority"
 
-mongoose.connect(DB_URL,{
+mongoose.connect(process.env.DB_URL,{
     useNewUrlParser : true,
     useUnifiedTopology : true
 }).then(()=>{console.log(`Database Connected !!`)})
