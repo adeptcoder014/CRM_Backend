5adf7df7cd918f2fe9959d288b26ec3d4fdb810e
Nischal <gupta.nischal014@gmail.com>
2022-11-12 18:36:38 +0530

    Started building the Backend!

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 //===========================
-const DB="mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority"
+const DB =
+  "mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority";
 // console.log("---<>",process.env.DB_URL)
-mongoose.connect(DB,{
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}).then(()=>{console.log(`Database Connected !!`)})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`${process.env.DB_URL} Database Connected `);
+  });
