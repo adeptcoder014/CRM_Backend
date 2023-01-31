964d7487701b2d569895c84bdae84fc1ffb87d5f
Nischal <gupta.nischal014@gmail.com>
2023-01-31 15:16:37 +0530

    database migrated to waqar

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 //===========================
-const DB =
-  "mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority";
+const DB = "mongodb+srv://Ahmed:waqar123@cluster0.te8fb8e.mongodb.net/?retryWrites=true&w=majority"
+// const DB =
+//   "mongodb+srv://User_369:TonyStark007@cluster0.37ajaix.mongodb.net/CRM?retryWrites=true&w=majority";
+
 // console.log("---<>",process.env.DB_URL)
 mongoose
   .connect(DB, {
